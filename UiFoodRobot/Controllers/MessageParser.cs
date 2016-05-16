9fef4f5ab6a9f2e2b772d8c7b3b53dc6283b238d
horia <horia@MAYA>
2016-05-16 19:45:16 +0300

    Added class for persistence

---
+++ UiFoodRobot/Controllers/MessageParser.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UiFoodRobot
+{
+    internal class MessageParser
+    {
+        public static Message HandleMessage(Message m)
+        {
+            string rm = "";
+            string text = m.Text;
+            if (text == "/h" || text == "/help")
+                rm = "Help message";
+            else if (text == "/o" || text == "/order")
+                rm = "What will it be?";
+            else if (text == "/m" || text == "/menu")
+            {
+                rm = "Here's the menu:\n";
+                rm += "1. No menu\n";
+                rm += "2. You're fat!\n";
+            }
+            else rm = "No comprendes, senor!";
+
+            return m.CreateReplyMessage(rm);
+        }
+
+       
+
+        public static Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+                Message reply = message.CreateReplyMessage("Hello! Type /h or /help for help, /m for menu, /o to order");
+                return reply;
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+                return message.CreateReplyMessage("Type /h or /help for help, /m for menu, /o to order");
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+                return message.CreateReplyMessage("Hasta la vista, baby!");
+
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+                return message.CreateReplyMessage("Buh bye!");
+            }
+
+            return null;
+        }
+    }
+}
--- UiFoodRobot/Controllers/MessagesController.cs
+++ UiFoodRobot/Controllers/MessagesController.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using UiFoodRobot;
 
-namespace MondayFoodRobot
+namespace UiFoodRobot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
@@ -17,68 +17,13 @@ namespace MondayFoodRobot
             if (message.Type == "Message")
             {
                 // return our reply to the user
-                return HandleMessage(message);
+                return MessageParser.HandleMessage(message);
             }
             else
             {
-                return HandleSystemMessage(message);
+                return MessageParser.HandleSystemMessage(message);
             }
         }
-        private Message HandleMessage(Message m)
-        {
-            string rm = "";
-            string text = m.Text;
-            if (text == "/h" || text == "/help")
-                rm = "Help message";
-            else if (text == "/o" || text == "/order")
-                rm = "What will it be?";
-            else if (text == "/m" || text == "/menu")
-            {
-                rm = "Here's the menu:\n";
-                rm += "1. No menu\n";
-                rm += "2. You're fat!\n";
-            }
-            else rm = "No comprendes, senor!";
-
-            return m.CreateReplyMessage(rm);
-        }
-
-        private Message HandleSystemMessage(Message message)
-        {
-            if (message.Type == "Ping")
-            {
-                Message reply = message.CreateReplyMessage();
-                reply.Type = "Ping";
-                return reply;
-            }
-            else if (message.Type == "DeleteUserData")
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == "BotAddedToConversation")
-            {
-                Message reply = message.CreateReplyMessage("Hello! Type /h or /help for help, /m for menu, /o to order");
-                return reply;
-            }
-            else if (message.Type == "BotRemovedFromConversation")
-            {
-            }
-            else if (message.Type == "UserAddedToConversation")
-            {
-                return message.CreateReplyMessage("Type /h or /help for help, /m for menu, /o to order");
-            }
-            else if (message.Type == "UserRemovedFromConversation")
-            {
-                return message.CreateReplyMessage("Hasta la vista, baby!");
-
-            }
-            else if (message.Type == "EndOfConversation")
-            {
-                return message.CreateReplyMessage("Buh bye!");
-            }
-
-            return null;
-        }
+      
     }
 }
